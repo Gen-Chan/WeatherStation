01be1229259eebfe9cee3443a544b2ce26adebb9
Gen-Chan <60892170+Gen-Chan@users.noreply.github.com>
2026-08-06 22:25:10 +0200

    Create config.py
    customize it for your own envoierment

---
+++ script/config.py
@@ -0,0 +1,17 @@
+# Pinout
+sclPin = 5
+sdaPin = 4
+
+# Timer between to datapoints
+interval = 360
+
+# Credentials for the WiFi connection
+wlan_ssid = ""
+wlan_pass = ""
+
+# Credentials for the Basic-Auth with Prometheus
+auth_user = ""
+auth_pass = ""
+
+# Serverendpoint to comunicat with
+server_uri = ""
